8620869c60abdc0a5707886c6b8e1110df02941c
Le-Yo <deoleyo@gmail.com>
2023-10-12 19:20:29 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const path= require('path');
+
+
+
+const MongoDB ='mongodb+srv://deoleyo:Kasongi2014@cluster0.heaqntp.mongodb.net/';
+console.log(MongoDB);
+
+ mongoose.set('strictQuery', true);
+
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(MongoDB, {
+            useUnifiedTopology: true,
+        });
+        console.log("MongoDB is connected");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    }
+}
+
+connectDB();
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+// app.use(express.static('public'));
+app.use('/static', express.static(path.join(__dirname,'public')));
+
+
+app.get('/', async (req, res) => {
+    res.render('index'); 
+});
+
+app.listen(8000, () => {
+    console.log('App is running on port 8000');
+});
