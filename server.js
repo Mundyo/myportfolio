3656ec1231adb5d78637035364726145bddf05e5
Le-Yo <deoleyo@gmail.com>
2023-10-12 21:13:05 -0500

    app.use..

--- server.js
+++ server.js
@@ -26,8 +26,8 @@ connectDB();
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
-// app.use(express.static('public'));
-app.use('/static', express.static(path.join(__dirname,'public')));
+app.use(express.static('public'));
+// app.use('/static', express.static(path.join(__dirname,'public')));
 
 
 app.get('/', async (req, res) => {
