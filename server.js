e687a0a175562cfae2f8facadcaca74da16baf0c
Le-Yo <deoleyo@gmail.com>
2023-10-20 12:26:22 -0500

    profile text

--- server.js
+++ server.js
@@ -7,24 +7,6 @@ const bodyParser = require("body-parser");
 
 
 
-// const MongoDB ='mongodb+srv://deoleyo:Kasongi2014@cluster0.heaqntp.mongodb.net/';
-// console.log(MongoDB);
-
-//  mongoose.set('strictQuery', true);
-
-
-// const connectDB = async () => {
-//     try {
-//         await mongoose.connect(MongoDB, {
-//             useUnifiedTopology: true,
-//         });
-//         console.log("MongoDB is connected");
-//     } catch (error) {
-//         console.error("Error connecting to MongoDB:", error);
-//     }
-// }
-
-// connectDB();
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
@@ -32,12 +14,8 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(morgan("tiny"));
 app.use(express.json());
-// app.use('/static', express.static(path.join(__dirname,'public')));
 
 
-// app.get('/', async (req, res) => {
-//     res.render('mundyo/index'); 
-// });
 app.get("/", async (req, res) => {
     try {
        
